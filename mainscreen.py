f1c3fb51a3018dea823a07a34042935f92a58f4b
FIIckSesh <47919704+FIIckSesh@users.noreply.github.com>
2021-03-29 23:37:52 +0300

    commit last

--- mainscreen.py
+++ mainscreen.py
@@ -11,11 +11,20 @@ class MainScreen(QtWidgets.QMainWindow, ui.mainscreen.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
-        self.GenerateButton.clicked.connect(self.f)
-        self.encryptButton.clicked.connect(self.f)
-        self.decipherButton.clicked.connect(self.f)
+        self.GenerateButton.clicked.connect(self.generate)
+        self.encryptButton.clicked.connect(self.encrypt)
+        self.decipherButton.clicked.connect(self.encrypt)
 
-    def f(self):
+        self.p = None
+        self.q = None
+        self.n = None
+        self.pm = None
+        self.qm = None
+        self.f = None
+        self.e = None
+        self.d = None
+
+    def generate(self):
         self.p = rd.rand()
         self.q = rd.rand()
         self.n = self.p.mul(self.q)
@@ -23,20 +32,17 @@ class MainScreen(QtWidgets.QMainWindow, ui.mainscreen.Ui_MainWindow):
         self.qm = self.q.mul(bg(1))
         self.f = self.pm.mul(self.qm)
         self.e = 199
+        self.d = bg(5)
+        #self.d = self.generateD(self.e, self.f)
 
         self.eEdit.setText(str(self.e))
         self.openEdit.setText("{" + str(self.e) + ", " + self.n.str() + "}")
-        self.d = self.generateD(self.e, self.f)
-        self.closeEdit.setText("{" + str(self.d) + ", " + self.n.str() + "}")
+        self.closeEdit.setText("{" + self.d.str() + ", " + self.n.str() + "}")
 
-    def generateD(self, e, f):
-        d = 1
-        print((bg(d).mul(bg(e))).mod(f).equals(bg(1)))
-        while (bg(d).mul(bg(e))).mod(f).equals(bg(1)) == False:
-            d += 1
+    def encrypt(self):
+        pass
 
-            print(d, " ", (bg(d).mul(bg(e))).mod(f).str())
-        return d
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
